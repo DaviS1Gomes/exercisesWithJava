d81ca835b7eae18ed863f0e574106bc0fd36d4cc
Davi Gomes <daviszgomes@gmail.com>
2024-05-09 18:25:16 -0300

    Fixing Duplicate Archives(Boolean Exercises)

--- BooleansExercises/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Faça um algoritmo que leia dois valores booleanos
-        // (lógicos) e determine se ambos são VERDADEIRO ou FALSO.
-
-
-    }
-}
